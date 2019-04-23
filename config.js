b2846e13400b6ec511c9fdf5f32edcb63de3e52d
bacti <hai.tranminh@gameloft.com>
2019-04-23 16:36:06 +0700

    +

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports =
 {
-    SERVER_DOMAIN: '35.197.137.94',
-    SERVER_PORT: 4433,
-    CLIENT_PORT: 8080,
+    // SERVER_DOMAIN: '35.197.137.94',
+    SERVER_DOMAIN: '34.65.56.126',
+    SERVER_PORT: 433,
+    CLIENT_PORT: 80,
 }
